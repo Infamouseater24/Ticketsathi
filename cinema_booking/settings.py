2dab84a3c1e58148616ec43ec62dfce50b95c7fe
Ubuntu <ubuntu@ip-172-31-47-76.ap-south-1.compute.internal>
2026-05-09 06:39:07 +0000

    Sync production server changes

--- cinema_booking/settings.py
+++ cinema_booking/settings.py
@@ -30,8 +30,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
-
+ALLOWED_HOSTS = ['65.0.81.229', 'aanandabastola.com.np']
 
 
 # Application definition
@@ -128,6 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
